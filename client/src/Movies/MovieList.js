68c995a23b8d2d3c2a5d7cfa5283a907a9c6b381
trodriguez89 <trodriguez89@gmail.com>
2019-11-12 11:56:54 -0800

    mvp wip

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import {Link} from "react-router-dom";
 import axios from 'axios';
 
 const MovieList = props => {
@@ -8,6 +9,7 @@ const MovieList = props => {
       axios
         .get('http://localhost:5000/api/movies')
         .then(response => {
+          console.log(response)
           setMovies(response.data);
         })
         .catch(error => {
@@ -21,7 +23,9 @@ const MovieList = props => {
   return (
     <div className="movie-list">
       {movies.map(movie => (
+        <Link to ={`/movies/${movie.id}`}>
         <MovieDetails key={movie.id} movie={movie} />
+        </Link>
       ))}
     </div>
   );
